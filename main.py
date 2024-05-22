b8feec928619c8174bc0a45bf07f3f4189cd540e
Nick442240 <chervon211258@gmail.com>
2024-05-22 15:08:28 +0300

    changed picture poop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-
 import random
 
 pygame.init()
@@ -12,19 +11,19 @@ pygame.display.set_caption('Игра Тир')
 icon = pygame.image.load('image/Тир.jpeg')
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load('image/target.png')
+target_img = pygame.image.load('image/poop.png')
 target_width = 80
 target_height = 80
 
-target_x = random.randint(0, SCREEN_WIDTH-target_width)
-target_y = random.randint(0,SCREEN_HEIGHT-target_height)
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 hits = 0  # Счетчик попаданий
 start_time = pygame.time.get_ticks()  # Начальное время
 
-
+clock = pygame.time.Clock()
 running = True
 
 while running:
@@ -34,25 +33,22 @@ while running:
         print(f'Количество попаданий за минуту: {hits}')
         continue
 
-    screen.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            if target_x<mouse_x<target_x+target_width and target_y<mouse_y<target_y+target_height:
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
                 # Изменить цвет при попадании
                 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-                hits += 1 # Увеличиваем счетчик попаданий
+                hits += 1  # Увеличиваем счетчик попаданий
 
-
-    screen.blit(target_img,(target_x, target_y))
+    screen.fill(color)
+    screen.blit(target_img, (target_x, target_y))
 
     pygame.display.update()
+    clock.tick(60)  # Ограничиваем FPS до 60 кадров в секунду
 
 pygame.quit()
-
- 
-
